a040576e98f39dc8a02fbdd574a1f5b170aa4120
bintikholifah <bintikholifah1616@gmail.com>
2022-03-23 15:14:10 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Contacts',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const DetailScreen(),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget> [
+            Container(
+                margin: EdgeInsets.only(top: 16.0),
+                child: const Text(
+                  "Simpang Lima Gumul",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+            ),//Title here
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: const <Widget>[
+                      Icon(Icons.calendar_today),
+                      Text('Open Everyday'),
+                    ],
+                  ),
+                ],
+              ),
+            ),//
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: const Text(
+                '     The Simpang Lima Gumul Monument or commonly abbreviated as SLG is one of the buildings that has become an icon of Kediri Regency which looks like the Arc de Triomphe in Paris, France. The SLG was built in 2003 and inaugurated in 2008, which was initiated by the then Regent of Kediri, Sutrisno.',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),//Description here
+          ],
+        ),
+      ),
+    );
+  }
+}
